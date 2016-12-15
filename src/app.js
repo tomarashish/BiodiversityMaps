eeb8dc4472c5f67b3235373923bba4bc414199d2
tomarashish <aashish.dyp@gmail.com>
2016-12-15 11:26:13 +0100

    add custom icon

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@
 //https://github.com/jalbertbowden/leafletjs-kit/tree/master/dvf
 //https://www.patrick-wied.at/static/heatmapjs/examples.html
 //https://bl.ocks.org/cjrd/6863459
-
-
+// icons for markers
+//https://www.iconfinder.com/icons/1110928/beetle_bug_fly_insect_insects_pest_icon#size=128
+//http://www.flaticon.com/free-icons/insect_223
 
 //file upload 
 var raw_data;
@@ -79,12 +80,18 @@ function init(collection){
     d3.select("#load-data").style("visibility","hidden").style("display", "none");
     d3.select("#show-viz").style("visibility","visible")
     
+	// Parsing data into json format
+	collection.forEach(function(d){
+			d.coordinates = JSON.parse(d.coordinates);
+		})
+	
 	//http://ivansanchez.gitlab.io/Leaflet.GridLayer.FadeOut/demo.html
+	//https://leaflet-extras.github.io/leaflet-providers/preview/
 	//Adding tiles to map
     var tile1 = L.tileLayer('http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', {
 				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ',noWrap : true } );
     
-		tile2 = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
+		tile2 = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 					attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS'
 					}),
 		
@@ -101,7 +108,7 @@ function init(collection){
 	
     var map = L.map( 'map', {
         layers:[tile1],
-        center: [.1, .1],
+        center: [1, 1],
         minZoom: 3,
         zoom:1,
         maxZoom: 15
@@ -109,7 +116,7 @@ function init(collection){
         //inertia: false
     });
 
-
+	
 	// layer control
 		var layerControlItems = {
 		  "<div class='layer-titles'> OSM Landscape </div>": tile1,
@@ -118,31 +125,47 @@ function init(collection){
 		  "<div class='layer-titles'> Stamen Toner Lite </div>":tile4
 		};
 		
-		L.control.layers(layerControlItems).addTo(map);
+	L.control.layers(layerControlItems).addTo(map);
 
+	// Setting custom icon marker 
+	var myIcon = L.icon({
+		iconUrl: "../data/images/beetle1.png",
+		iconSize:[35,35],
+	})
 	
 	
+	//Marker clustering
+	//var markerClusters = L.markerClusterGroup();
+	
+	//Setting custom icon marker to map
+	// Using L.marker to get cordinates and set the icon
+	collection.forEach(function(d) {
+		
+		var m = L.marker( [d.coordinates[0], d.coordinates[1]], {icon: myIcon} );
+		
+	  //for marker icon 
+		map.addLayer(m);
+		
+	 //For marker cluster	
+	 //markerClusters.addLayer( m );
+	})
+	
+	//map.addLayer(markerClusters);
+	
+
+						   
     /* Initialize the SVG layer */
 	//L.svg().addTo(map); // witth new leaflet version
-   map._initPathRoot()    
+   //map._initPathRoot()    
     
 	/* We simply pick up the SVG from the map object */
-	var svg = d3.select("#map").select("svg"),
+	/*var svg = d3.select("#map").select("svg"),
 	g = svg.append("g");
 
-		
-	collection.forEach(function(d){
-			d.coordinates = JSON.parse(d.coordinates);
-		})
-	
 	collection.forEach(function(d) {
 		d.LatLng = new L.LatLng(d.coordinates[0],
 								d.coordinates[1])
 	})
-	
-	
-
-	
 	var feature = g.selectAll("circle")
 		.data(collection)
 		.enter().append("circle")
@@ -171,7 +194,7 @@ function init(collection){
 		)
 	} // end of update 
        
-	
+	*/
 	/*
 	function doImage(err, canvas) {
             var img = document.createElement('img');
